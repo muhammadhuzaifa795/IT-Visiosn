8ec048a03cf03b4e1c7305239b7ab6fca735aecb
Rajeel Siddiqui <159783237+RajeelSiddiqui1@users.noreply.github.com>
2025-09-24 12:55:59 +0500

    push

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -8,17 +8,20 @@ export async function getRecommendedUsers(req, res) {
 
     const recommendedUsers = await User.find({
       $and: [
-        { _id: { $ne: currentUserId } }, //exclude current user
+        { _id: { $ne: currentUserId } }, // exclude current user
         { _id: { $nin: currentUser.friends } }, // exclude current user's friends
         { isOnboarded: true },
+        { role: { $ne: "admin" } }, // exclude admins ✅
       ],
     });
+
     res.status(200).json(recommendedUsers);
   } catch (error) {
     console.error("Error in getRecommendedUsers controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
+
 
 export async function getMyFriends(req, res) {
   try {
