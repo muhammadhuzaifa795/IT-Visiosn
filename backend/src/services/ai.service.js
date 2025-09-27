08530276a7987a0463f3e801e63ef048b30ff9dc
Rajeel Siddiqui <159783237+RajeelSiddiqui1@users.noreply.github.com>
2025-09-28 01:04:25 +0500

    push

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -6,7 +6,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 async function generateContent(prompt) {
   try {
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.0-flash",
+      model: "gemini-2.5-flash",
       systemInstruction: {
         role: "system",
         parts: [
